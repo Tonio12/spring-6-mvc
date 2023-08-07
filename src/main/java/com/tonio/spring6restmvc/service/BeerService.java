4ed5b96f090f28103740de8ec1d7c4217a7c8710
Administrator <nelson.antonio.an@gmail.com>
2023-08-07 10:59:13 +0000

    Get All Beers And Get Beer By ID endpoints Implemented

---
+++ src/main/java/com/tonio/spring6restmvc/service/BeerService.java
@@ -0,0 +1,12 @@
+package com.tonio.spring6restmvc.service;
+
+import com.tonio.spring6restmvc.model.Beer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BeerService {
+    Beer getBeerById(UUID id);
+
+    List<Beer> listBeers();
+}
